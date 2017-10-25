178c412b1b88989a549d245c11a5017fc7a57397
aloyschen <813971830@qq.com>
2017-10-25 11:21:55 +0800

    model load

--- FCN_train.py
+++ FCN_train.py
@@ -62,3 +62,73 @@ def vgg_net(weights, image):
     return net
 
 
+def segmentation(image, keep_prob):
+    """
+    图像语义分割模型定义
+    Parameters
+    ----------
+        image: 输入图像，每个通道的像素值为0到255
+        keep_prob:
+    Returns
+    -------
+
+    """
+    print("setting up vgg initialized conv layers ...")
+    model_data = utils.get_model_data(FLAGS.model_dir)
+
+    mean = model_data['normalization'][0][0][0]
+    mean_pixel = np.mean(mean, axis=(0, 1))
+
+    weights = np.squeeze(model_data['layers'])
+
+    processed_image = utils.process_image(image, mean_pixel)
+
+    with tf.variable_scope("inference"):
+        image_net = vgg_net(weights, processed_image)
+        conv_final_layer = image_net["conv5_3"]
+
+        pool5 = utils.max_pool_2x2(conv_final_layer)
+
+        W6 = utils.weight_variable([7, 7, 512, 4096], name="W6")
+        b6 = utils.bias_variable([4096], name="b6")
+        conv6 = utils.conv2d_basic(pool5, W6, b6)
+        relu6 = tf.nn.relu(conv6, name="relu6")
+        if FLAGS.debug:
+            utils.add_activation_summary(relu6)
+        relu_dropout6 = tf.nn.dropout(relu6, keep_prob=keep_prob)
+
+        W7 = utils.weight_variable([1, 1, 4096, 4096], name="W7")
+        b7 = utils.bias_variable([4096], name="b7")
+        conv7 = utils.conv2d_basic(relu_dropout6, W7, b7)
+        relu7 = tf.nn.relu(conv7, name="relu7")
+        if FLAGS.debug:
+            utils.add_activation_summary(relu7)
+        relu_dropout7 = tf.nn.dropout(relu7, keep_prob=keep_prob)
+
+        W8 = utils.weight_variable([1, 1, 4096, NUM_OF_CLASSESS], name="W8")
+        b8 = utils.bias_variable([NUM_OF_CLASSESS], name="b8")
+        conv8 = utils.conv2d_basic(relu_dropout7, W8, b8)
+        # annotation_pred1 = tf.argmax(conv8, dimension=3, name="prediction1")
+
+        # now to upscale to actual image size
+        deconv_shape1 = image_net["pool4"].get_shape()
+        W_t1 = utils.weight_variable([4, 4, deconv_shape1[3].value, NUM_OF_CLASSESS], name="W_t1")
+        b_t1 = utils.bias_variable([deconv_shape1[3].value], name="b_t1")
+        conv_t1 = utils.conv2d_transpose_strided(conv8, W_t1, b_t1, output_shape=tf.shape(image_net["pool4"]))
+        fuse_1 = tf.add(conv_t1, image_net["pool4"], name="fuse_1")
+
+        deconv_shape2 = image_net["pool3"].get_shape()
+        W_t2 = utils.weight_variable([4, 4, deconv_shape2[3].value, deconv_shape1[3].value], name="W_t2")
+        b_t2 = utils.bias_variable([deconv_shape2[3].value], name="b_t2")
+        conv_t2 = utils.conv2d_transpose_strided(fuse_1, W_t2, b_t2, output_shape=tf.shape(image_net["pool3"]))
+        fuse_2 = tf.add(conv_t2, image_net["pool3"], name="fuse_2")
+
+        shape = tf.shape(image)
+        deconv_shape3 = tf.stack([shape[0], shape[1], shape[2], NUM_OF_CLASSESS])
+        W_t3 = utils.weight_variable([16, 16, NUM_OF_CLASSESS, deconv_shape2[3].value], name="W_t3")
+        b_t3 = utils.bias_variable([NUM_OF_CLASSESS], name="b_t3")
+        conv_t3 = utils.conv2d_transpose_strided(fuse_2, W_t3, b_t3, output_shape=deconv_shape3, stride=8)
+
+        annotation_pred = tf.argmax(conv_t3, dimension=3, name="prediction")
+
+    return tf.expand_dims(annotation_pred, dim=3), conv_t3
--- utils.py
+++ utils.py
@@ -8,37 +8,18 @@ import zipfile
 import scipy.io
 
 
-def get_model_data(dir_path, model_url):
-    maybe_download_and_extract(dir_path, model_url)
-    filename = model_url.split("/")[-1]
-    filepath = os.path.join(dir_path, filename)
-    if not os.path.exists(filepath):
-        raise IOError("VGG Model not found!")
-    data = scipy.io.loadmat(filepath)
+def get_model_data(dir_path):
+    """
+    读取vgg模型的mat文件
+    Parameters
+    ----------
+        dir_path: 存放模型的路径
+    Returns
+    -------
+        data: 读取的模型文件
+    """
+    data = scipy.io.loadmat(dir_path)
     return data
-
-
-def maybe_download_and_extract(dir_path, url_name, is_tarfile=False, is_zipfile=False):
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    filename = url_name.split('/')[-1]
-    filepath = os.path.join(dir_path, filename)
-    if not os.path.exists(filepath):
-        def _progress(count, block_size, total_size):
-            sys.stdout.write(
-                '\r>> Downloading %s %.1f%%' % (filename, float(count * block_size) / float(total_size) * 100.0))
-            sys.stdout.flush()
-
-        filepath, _ = urllib.request.urlretrieve(url_name, filepath, reporthook=_progress)
-        print()
-        statinfo = os.stat(filepath)
-        print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
-        if is_tarfile:
-            tarfile.open(filepath, 'r:gz').extractall(dir_path)
-        elif is_zipfile:
-            with zipfile.ZipFile(filepath) as zf:
-                zip_dir = zf.namelist()[0]
-                zf.extractall(dir_path)
 
 
 def save_image(image, save_dir, name, mean=None):
